279356c7482ffb4eac0243c5e20095e3139a7dd0
fcolona <felipedev202@gmail.com>
2025-02-13 17:40:11 -0300

    (feat): add file handling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
 
 int main(){
-    std::cout << "Hello World" << std::endl;
+    std::ifstream file;
+    file.open("test.txt");
+    if(!file.is_open()) throw std::invalid_argument("Could not open file");
+    
+    std::string line;
+    while(std::getline(file, line)){
+        std::cout << line << "\n";
+    }
+    file.close();
+
     return 0;
 }
